4dff63668a93394feb4307e8f994884bc256f137
Jonathan Willian <47341973+JonathanWCP@users.noreply.github.com>
2019-04-21 11:55:30 -0300

    Add V1.1

--- SO_Processos02/src/controller/ProcessosController.java
+++ SO_Processos02/src/controller/ProcessosController.java
@@ -31,46 +31,95 @@ public class ProcessosController {
 				JOptionPane.showMessageDialog(null, e1.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
 			}
 
+		} else {
+			buffer.append(processo);
+
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+			}
+
 		}
 	}
 
 	public void leProcesso(String so) {
-		String processo = "TASKLIST /FO TABLE";
-		try {
-			Process pr = Runtime.getRuntime().exec(processo);
-			InputStream fluxo = pr.getInputStream(); // vários sei lá o que dentro do pr-processo
-			InputStreamReader leitor = new InputStreamReader(fluxo);
-			BufferedReader buffer = new BufferedReader(leitor);
-			String linha = buffer.readLine();
+		if (so.contains("Windows")) {
+			String processo = "TASKLIST /FO TABLE";
+			try {
+				Process pr = Runtime.getRuntime().exec(processo);
+				InputStream fluxo = pr.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
 
-			while (linha != null) {
-				System.out.println(linha);
-				linha = buffer.readLine(); // usa o buffer para ler uma linha e joga na mesma
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine(); // usa o buffer para ler uma linha e joga na mesma
+				}
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
+		} else {
+			try {
+				String processo = "ps -a";
+				Process pr = Runtime.getRuntime().exec(processo);
+				InputStream fluxo = pr.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine(); // usa o buffer para ler uma linha e joga na mesma
+				}
+
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			}
+
 		}
 	}
 
 	public void mataProcesso(String so, String info) { // matar processo por PID e pelo nome do processo
-		StringBuffer buffer = new StringBuffer();
-		String cmdNome = "TASKKILL /IM "; // matar pelo nome
-		String cmdPid = "TASKKILL /PID "; // matar pelo PID
-		int pid = 0;
+		if (so.contains("Windows")) {
+			StringBuffer buffer = new StringBuffer();
+			String cmdNome = "TASKKILL /IM "; // matar pelo nome
+			String cmdPid = "TASKKILL /PID "; // matar pelo PID
+			int pid = 0;
 
-		try {
-			pid = Integer.parseInt(info);
-			buffer.append(cmdPid);
-			buffer.append(pid);
-		} catch (NumberFormatException e) {
-			if (!info.contains(".exe")) {
-				info += ".exe";
+			try {
+				pid = Integer.parseInt(info);
+				buffer.append(cmdPid);
+				buffer.append(pid);
+			} catch (NumberFormatException e) {
+				if (!info.contains(".exe")) {
+					info += ".exe";
+				}
+				buffer.append(cmdNome);
+				buffer.append(info);
+			} finally {
+				chamaProcesso(buffer.toString());
 			}
-			buffer.append(cmdNome);
-			buffer.append(info);
-		} finally {
-			chamaProcesso(buffer.toString());
+		} else if (so.contains("Linux")) {
+			StringBuffer buffer = new StringBuffer();
+			String cmdPid = "kill "; // matar pelo PID
+			String cmdNome = "pkill "; // matar pelo nome
+			int pid = 0;
+
+			try {
+				pid = Integer.parseInt(info);
+				buffer.append(cmdPid);
+				buffer.append(pid);
+			} catch (NumberFormatException e) {
+				buffer.append(cmdNome);
+				buffer.append(info);
+			} finally {
+				chamaProcesso(buffer.toString());
+			}
+
 		}
 	}
 }
