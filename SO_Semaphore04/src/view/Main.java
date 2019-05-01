33f281fd8e8e34d84fa045da83e9ff02a4386bb8
Jonathan Willian <47341973+JonathanWCP@users.noreply.github.com>
2019-04-30 23:38:27 -0300

    Add files via upload

--- SO_Semaphore04/src/view/Main.java
+++ SO_Semaphore04/src/view/Main.java
@@ -1,4 +1,15 @@
-/*Objetivo:     ...
+/*Objetivo:     Um banco deve controlar Saques e Depósitos.
+				O sistema pode permitir um Saque e um Depósito
+				Simultâneos, mas nunca 2 Saques ou 2 Depósitos
+				Simultâneos.
+				Para calcular a transação (Saque ou Depósito), o método deve
+				receber o código da conta, o saldo da conta e o valor a ser
+				transacionado.
+				Deve-se montar um sistema que deve considerar que 20
+				transações simultâneas serão enviadas ao sistema
+				(aleatoriamente essas transações podem ser qualquer uma das
+				opções) e tratar todas as transações, de acordo com as regras
+				acima.
                 
 				Este codigo fonte sera salvo no arquivo Main.java
    Programador: Jonathan Willian Castro Pinheiro
